55a7214953c7cbcc248212948010817d4744e617
Kashan Haider <kashan.hz999@gmail.com>
2022-12-23 23:35:23 +0530

    Buy NFT Added

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -5,7 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ETHDaddy is ERC721{
   address public owner;
-  uint256 public maxSupply=0;
+  uint256 public maxSupply;
+  uint256 public totalSupply;
 
   struct Domain{
     string name;
@@ -24,12 +25,36 @@ contract ETHDaddy is ERC721{
   }
 
   function list(string memory _name,uint256 _cost) public onlyOwner{
-
       maxSupply++;
       domains[maxSupply] = Domain(_name,_cost,false);
+  }
 
+  function mint(uint256 _id) public payable {
+      require(_id != 0);
+      require(_id <= maxSupply);
+      require(domains[_id].isOwned == false);
+      require(msg.value >= domains[_id].cost);
 
-
+      domains[_id].isOwned = true;
+      totalSupply++;
+      _safeMint(msg.sender,_id);
 
   }
+  function getDomain(uint256 _id) public view returns (Domain memory){
+    return domains[_id];
+  }
+
+  function getBalance() public view returns(uint256) {
+    return address(this).balance;
+  }
+
+   function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
+
+
+
+
+
 }
